984cdacf3c134e71ded59dc26f6ba84a08cc38f5
Winston Venderbush <winstonvenderbush@gmail.com>
2017-01-08 01:38:26 -0500

    skeleton app.py

--- app.py
+++ app.py
@@ -12,20 +12,43 @@ app.secret_key = 'secrets'
 
 @app.route("/", methods = ["POST", "GET"])
 def root():
-	# form = request.form
-	# if ("logval" in form):
-	# 	print session
-	# 	user = session["username"]
-	# 	session.pop(hashlib.sha256(user).hexdigest())
-	# 	session.pop("username")
-	# 	return render_template('main.html', title = "Login", message = "You have been logged out!", flag = "logout")
-	# if ("username" in session):
-	# 	return render_template('main.html', title = "Landing", message = "Welcome, " + session["username"] + "!", flag = "login")
-	# return render_template('main.html', title = "Login", message = "Enter your username and password:", flag = "logout")
-	return "hello";
+	form = request.form
+	if ("logout" in form):
+		user = session["username"]
+		session.pop("username")
+		return render_template('main.html', title = "Login", message = "You have been logged out!", flag = "logout")
+	if ("username" in session):
+		return render_template('main.html', title = "Landing", message = "Welcome, " + session["username"] + "!", flag = "login")
+	return render_template('main.html', title = "Login", message = "Enter your username and password:", flag = "logout")
+	return "Main Page";
 
 @app.route("/login", methods = ["POST", "GET"])
 def login():
+	form = request.form
+	if ("login" in form):
+		session["username"] = username;
+		return "Main Page";
+	return "Login Page";
+
+@app.route("/search", methods = ["POST", "GET"])
+def search():
+	return "Search Page";
+
+@app.route("/shop", methods = ["POST", "GET"])
+def search():
+	return "Shop Page";
+
+@app.route("/creategroup", methods = ["POST", "GET"])
+def search():
+	return "Create Group Page";
+
+@app.route("/dashboard", methods = ["POST", "GET"])
+def search():
+	return "Dashboard Page";
+
+@app.route("/blacklist", methods = ["POST", "GET"])
+def search():
+	return "Blacklist Page";
 
 
 if __name__ == "__main__":
