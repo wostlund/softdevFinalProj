2f4a9cc6cf9c305ce29e4b8e5d1969a449ee4d5d
Winston Venderbush <winstonvenderbush@gmail.com>
2017-01-09 14:52:41 -0500

    final additions

---
+++ utils/amazon.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import json, urllib2, datetime
+
+##############
+### AMAZON ###
+##############
+
+key = open("keys.txt", "r").read().strip().split("\n")[0]
+baseurl = """http://webservices.amazon.com/onca/xml?Service=AWSECommerceService&
+        	Operation=%s&
+        	AWSAccessKeyId=%s&
+        	AssociateTag=%s&
+        	SubscriptionId=%s&
+        	AssociateTag=%s&
+        	SearchIndex=%s&
+        	Keywords=%s&
+        	ResponseGroup=%s"""
