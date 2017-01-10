c0aab49cecbc641ecc7a3949a97314e58bb57421
Zzcnick <zzhen@stuy.edu>
2017-01-10 14:54:23 -0500

    data stuff

--- utils/auth.py
+++ utils/auth.py
@@ -12,12 +12,15 @@ def disconnect(db):
     db.commit()
     db.close()
 
+# Authorization Functions
+# ==========================================================================
 def login(username, password):
+    username = username.lower()
     if check_safe(username) < 0:
         return -2 # Invalid Username
     db = connect()
     c = db.cursor
-    req = "SELECT username FROM userdata WHERE username == %s"%(username)
+    req = "SELECT username FROM userdata WHERE username == '%s'"%(username)
     data = c.execute(req)
     if data: # User is registered.
         if data['password'] == hash(password):
@@ -26,13 +29,15 @@ def login(username, password):
     return -2 # Invalid Username
 
 def check_safe(username):
-    if (username.lower() == "drop tables"):
+    if (username == "drop tables"):
         return -2 # You think you're clever, don't you.
-    good = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890!@#$%^&*()"
+    good = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890"
     for char in username:
         if char not in good:
             return -1 # Invalid Character
     return 0 # Good
 
 def hash(password): # To Implement
-    return password + "hello"
+    password = password + "this-is-some-nice-salt-and-pepper"
+    password = hashlib.sha384(password).hexdigest()
+    return password
--- utils/data.py
+++ utils/data.py
@@ -5,10 +5,8 @@
 import sqlite3
 import csv
 
-# Table Initialization Functions
-# ==========================================================================
 def connect():
-    name = "./data/<TABLE NAME HERE>.db"
+    name = "./data.db"
     db = sqlite3.connect(name)
     return db
 
@@ -16,31 +14,47 @@ def disconnect(db):
     db.commit()
     db.close()
 
+# Table Initialization Functions
+# ==========================================================================
 def init():
     db = connect()
     c = db.cursor()
     # Creating Tables
-    # TO EDIT
-    cmd = "CREATE TABLE IF NOT EXISTS userdata (username TEXT, password TEXT)"
-    c.execute(cmd)
+    cmdlist = ["CREATE TABLE IF NOT EXISTS groups \
+               (groupid INTEGER, username TEXT, recipient TEXT)",
+               "CREATE TABLE IF NOT EXISTS groupdata \
+               (groupid INTEGER, groupname TEXT, members INTEGER)",
+               "CREATE TABLE IF NOT EXISTS userdata \
+               (username TEXT, password TEXT, name TEXT, email TEXT)",
+               "CREATE TABLE IF NOT EXISTS wishlists \
+               (username TEXT, itemname TEXT)",
+               "CREATE TABLE IF NOT EXISTS blacklists \
+               (username TEXT, ignoreuser TEXT, ignorename TEXT)",
+               "CREATE TABLE IF NOT EXISTS shoppinglists \
+               (username TEXT, itemname TEXT, link TEXT)"]
+    for cmd in cmdlist:
+        c.execute(cmd)
     disconnect(db)
 
 def reset():
     db = connect()
     c = db.cursor()
     # Delete Entries
-    # TO EDIT
-    cmd = "DELETE FROM userdata"
+    tablelist = ["groups", "groupdata", "userdata", "wishlists", "blacklists", "shoppinglists"]
+    for table in tablelist:
+        cmd = "DELETE FROM %s"%(table)
+        c.execute(cmd)
     disconnect(db)
     
-# Data Editing Functions
+# Database Editing Functions
 # ==========================================================================
-def add_user(username, password):
+def add_user(username, password, name, email):
     try:
         db = connect()
         c = db.cursor()
-        req = "INSERT INTO userdata VALUES (?,?)"
-        c.execute(req, (username,password))
+        req = "INSERT INTO userdata VALUES \
+               ('%s','%s','%s','%s')"%(username, password, name, email)
+        c.execute(req)
         disconnect(db)
         return True
     except:
@@ -49,4 +63,6 @@ def add_user(username, password):
 # Initialization
 # ==========================================================================
 if (__name__ == "__main__"):
+    reset()
     init()
+    
