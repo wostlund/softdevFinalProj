414057b325ec15cdeca7408a5cc88a79dcffec0c
Winston Venderbush <winstonvenderbush@gmail.com>
2017-01-11 15:12:56 -0500

    price control on etsy

--- utils/etsy.py
+++ utils/etsy.py
@@ -5,12 +5,12 @@ import json, urllib2, datetime
 ### ETSY ###
 ############
 
-key = open("keys.txt", "r").read().strip().split("\n")[1]
+key = open("../keys.txt", "r").read().strip().split("\n")[1]
 baseurl = "https://openapi.etsy.com/v2/"
 
 
 
-def search(terms, number): #add price controls!!
+def search(terms, number, price): #add price controls!!
 	urladd = "listings/active?keywords=" + terms + "&limit=" + str(number) + "&includes=Images:1&api_key=" + key
 	url = baseurl + urladd
 	curr = urllib2.urlopen(url)
@@ -24,8 +24,13 @@ def search(terms, number): #add price controls!!
 		newdict["description"] = i["description"]
 		newdict["category"] = i["category_path"]
 		newdict["price"] = i["price"]
+		intprice = int(float(newdict["price"]))
 		newdict["currency"] = i["currency_code"]
 		newdict["url"] = i["url"];
 		imgs = i["Images"][0]; #dictionary
 		newdict["imgurl"] = imgs["url_fullxfull"]
-		reslist.append(newdict)
+		if (intprice <= price):
+			reslist.append(newdict)
+	return reslist
+
+#print search("donkey", 25, 20)
