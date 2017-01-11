5bf6aba3c0b7135a6a2fca2fdf06250e8f4fa2f3
Winston Venderbush <winstonvenderbush@gmail.com>
2017-01-11 14:51:44 -0500

    edited app.py

--- utils/amazon.py
+++ utils/amazon.py
@@ -6,12 +6,38 @@ import json, urllib2, datetime
 ##############
 
 key = open("keys.txt", "r").read().strip().split("\n")[0]
-baseurl = """http://webservices.amazon.com/onca/xml?Service=AWSECommerceService&
-        	Operation=%s&
-        	AWSAccessKeyId=%s&
-        	AssociateTag=%s&
-        	SubscriptionId=%s&
-        	AssociateTag=%s&
-        	SearchIndex=%s&
-        	Keywords=%s&
-        	ResponseGroup=%s"""
+# baseurl = """http://webservices.amazon.com/onca/xml?Service=AWSECommerceService
+# 			&Operation=ItemSearch
+# 			&AWSAccessKeyId=""" + key + """
+# 			&Operation=ItemSearch
+# 			&IncludeReviewsSummary=True
+# 			&MaximumPrice=""" + price + """
+# 			&Keywords=""" + terms + """
+# 			&ResponseGroup=ItemAttributes,Offers,Images,Reviews,Variations
+# 			&Version=2013-08-01
+# 			&SearchIndex=All
+# 			&Sort=salesrank
+# 			"""
+			#&AssociateTag=mytag-20
+
+#test
+
+def search(terms, number, price): #price as a number (ie $20.00 is 2000)
+	baseurl = """http://webservices.amazon.com/onca/xml?Service=AWSECommerceService
+			&Operation=ItemSearch
+			&AWSAccessKeyId=""" + key + """
+			&Operation=ItemSearch
+			&IncludeReviewsSummary=True
+			&MaximumPrice=""" + str(price) + """
+			&Keywords=""" + terms + """
+			&ResponseGroup=ItemAttributes,Offers,Images,Reviews,Variations
+			&Version=2013-08-01
+			&SearchIndex=All
+			&Sort=salesrank
+			"""
+	print baseurl
+	# curr = urllib2.urlopen(url)
+	# req = curr.read()
+	# data = json.loads(req)
+
+search("donkey", 10, 2000)
