a801699b8877aba7c89ac72bc9bf8c64c6f3b5c7
Winston Venderbush <winstonvenderbush@gmail.com>
2017-01-13 15:01:51 -0500

    created keygen

--- utils/amazon.py
+++ utils/amazon.py
@@ -6,45 +6,14 @@ from amazonproduct import API
 ### AMAZON ###
 ##############
 
+#Dependencies: pip install python-amazon-product-api
+#Dependencies: pip install lxml
+
 api = API(locale='us')
 
 def search(terms, number, price):
 	print api.item_search('Blended', Keywords=terms);
 
-key = open("keys.txt", "r").read().strip().split("\n")[0]
-# baseurl = """http://webservices.amazon.com/onca/xml?Service=AWSECommerceService
-# 			&Operation=ItemSearch
-# 			&AWSAccessKeyId=""" + key + """
-# 			&Operation=ItemSearch
-# 			&IncludeReviewsSummary=True
-# 			&MaximumPrice=""" + price + """
-# 			&Keywords=""" + terms + """
-# 			&ResponseGroup=ItemAttributes,Offers,Images,Reviews,Variations
-# 			&Version=2013-08-01
-# 			&SearchIndex=All
-# 			&Sort=salesrank
-# 			"""
-			#&AssociateTag=mytag-20
-
-#test
-
-# def search(terms, number, price): #price as a number (ie $20.00 is 2000)
-# 	baseurl = """http://webservices.amazon.com/onca/xml?Service=AWSECommerceService
-# 			&Operation=ItemSearch
-# 			&AWSAccessKeyId=""" + key + """
-# 			&Operation=ItemSearch
-# 			&IncludeReviewsSummary=True
-# 			&MaximumPrice=""" + str(price) + """
-# 			&Keywords=""" + terms + """
-# 			&ResponseGroup=ItemAttributes,Offers,Images,Reviews,Variations
-# 			&Version=2013-08-01
-# 			&SearchIndex=All
-# 			&Sort=salesrank
-# 			"""
-# 	print baseurl
-# 	# curr = urllib2.urlopen(url)
-# 	# req = curr.read()
-# 	# data = json.loads(req)
 
 search("donkey", 10, 2000)
 
