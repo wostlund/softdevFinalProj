0147cbfec619a348cb27bdd01224cf005f8c4fe2
wostlund <wostlund@stuy.edu>
2017-01-13 14:11:50 -0500

    Added login and register part

---
+++ static/scripts/script.js
@@ -0,0 +1,9 @@
+function changeToLogin(){
+    document.getElementById("register").style.display = "none";
+    document.getElementById("login").style.display = "inherit";
+}
+
+function changeToRegister(){
+    document.getElementById("register").style.display = "inherit";
+    document.getElementById("login").style.display = "none";
+}
