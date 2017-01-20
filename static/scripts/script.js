3cce0af4affddf35a7e4dc4d08d912bae5b5736a
wostlund <wostlund@stuy.edu>
2017-01-20 16:50:08 -0500

    Fixed screwed up js

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,13 +1,11 @@
-function changeToLogin(){
- -    document.getElementById("hide").style.display = "none";
- -    //document.getElementById("disappear").style.display = "none";
- -    document.getElementById("show").style.display = "inherit";
- -    //document.getElementById("appear").style.display = "inherit";
- +    document.getElementById("register").style.display = "none";
- +    document.getElementById("login").style.display = "inherit";
+  function changeToRegister(){
+      document.getElementById("register").style.display = "none";
+      document.getElementById("login").style.display = "inherit";
   }
+
   
- +
+
   function changeToRegister(){
       document.getElementById("register").style.display = "inherit";
       document.getElementById("login").style.display = "none";
+  }
