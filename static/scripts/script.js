e1d7abe4d28871841f1e7c4edd4b2814d8042b5b
wostlund <wostlund@stuy.edu>
2017-01-20 15:00:23 -0500

    Fixed js, please tell me what specifically is wrong, if any other issues pop up

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,9 +1,8 @@
 function changeToLogin(){
-    document.getElementById("hide").style.display = "none";
-    //document.getElementById("disappear").style.display = "none";
-    document.getElementById("show").style.display = "inherit";
-    //document.getElementById("appear").style.display = "inherit";
+    document.getElementById("register").style.display = "none";
+    document.getElementById("login").style.display = "inherit";
 }
+
 
 function changeToRegister(){
     document.getElementById("register").style.display = "inherit";
